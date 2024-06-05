c473e306980d512c259d617c1ef9d7769ff8ad82
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-06-05 23:08:45 +0530

    react router

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+ 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className=' bg-green-600 p-4'> React Router</h1>
+      <h1 className=' bg-green-500'>Hello world </h1>
     </>
   )
 }
---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>User : 1</div>
+  )
+}
+
+export default User
