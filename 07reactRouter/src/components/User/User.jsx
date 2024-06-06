a60dd67d7edfe185de3a323f54190bdb9eb23abb
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-06-06 11:30:16 +0530

    react router

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
+import { useParams } from "react-router-dom";
 function User() {
+    const {id} =useParams()
   return (
-    <div>User : 1</div>
+    <div className=" bg-gray-600 text-white text-center text-3xl p-4">User : {id}</div>
   )
 }
 
