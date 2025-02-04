8c27998190c400ac636eb9090704e66763d41721
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-02-04 23:33:37 +0530

    Initialize React app with Vite and basic structure

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,28 +1,59 @@
-function customRender(reactElement, container) {
-  /*const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML=reactElement.Children
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
-    container.appendChild(domElement)
-    */
+// function customRender(reactElement, container) {
+//   /*const domElement = document.createElement(reactElement.type)
+//     domElement.innerHTML=reactElement.Children
+//     domElement.setAttribute('href',reactElement.props.href)
+//     domElement.setAttribute('target',reactElement.props.target)
+//     container.appendChild(domElement)
+//     */
 
-  const domElement = document.createElement(reactElement.type);
-  document.createElement(reactElement.type);
-  domElement.innerHTML = reactElement.Children;
-  for (const prop in reactElement.props) {
-    if (prop === 'Children') continue;
-    domElement.setAttribute(prop, reactElement.props[prop]);
-  }
-  container.appendChild(domElement);
-}
+//   const domElement = document.createElement(reactElement.type);
+//   document.createElement(reactElement.type);
+//   domElement.innerHTML = reactElement.Children;
+//   for (const prop in reactElement.props) {
+//     if (prop === 'Children') continue;
+//     domElement.setAttribute(prop, reactElement.props[prop]);
+//   }
+//   container.appendChild(domElement);
+// }
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   Children: "Click me to visit google",
+// };
+// const mainContainer = document.querySelector("#root");
+
+// customRender(reactElement, mainContainer);
+
+const mainContainer = document.getElementById("root");
+
 const reactElement = {
-  type: "a",
-  props: {
-    href: "https://google.com",
-    target: "_blank",
-  },
-  Children: "Click me to visit google",
+    type: "a",
+    props: {
+        href: "https://google.com",
+        target: "_blank",
+    },
+    childern: "Click me to visit google",
 };
-const mainContainer = document.querySelector("#root");
 
+const customRender = (reactElement, mainContainer) => {
+    // not suitable for multiple attributes of an element
+    /*
+  const domElement = document.createElement(reactElement.type)
+  domElement.innerHTML = reactElement.childern
+  domElement.setAttribute('href',reactElement.props.href)
+  domElement.setAttribute('target',reactElement.props.target)
+  mainContainer.appendChild(domElement)
+  */
+
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.childern;
+    for (const prop in reactElement.props) {
+        if (prop === 'childern') continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
+    mainContainer.appendChild(domElement);
+};
 customRender(reactElement, mainContainer);
